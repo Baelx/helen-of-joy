adf83d8aff9780c1d69837ac1cf948aa15f36d35
Alex <awintschel@gmail.com>
2017-05-17 18:12:50 -0400

    pushing sketch modifications

--- assets/js/createspace.js
+++ assets/js/createspace.js
@@ -3,5 +3,4 @@ var wheight = window.innerHeight + "px";
 function createSpace() {
     document.getElementById("hero").style.height = wheight;
 }
-console.log(wheight);
 createSpace();
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,17 +1,22 @@
 var img;  // Declare variable 'img'.
 var sw = window.innerWidth;
 var sh = window.innerHeight;
-var cntrimg = -((sh - 400) / 1.55);
+var imgh = sh * 1.5389558232931726;
+var drawheight = imgh * -0.25;
 
 function setup() {
   var cnv = createCanvas(sw, sh);
-  cnv.parent('hero');
-  img = loadImage("/assets/img/helenofjoytall.jpg");  // Load the image
+  cnv.parent('hero'); 
+  img = loadImage("assets/img/helenofjoytall.jpg");
 }
 
 function draw() {
-  // Displays the image exactly as wide as the viewport and keeps the aspect ratio of the image(1 over 1.5)
-  image(img, 0, cntrimg, sw, sw * 1.5);
+  // Displays the image exactly as wide as the viewport and keeps the aspect ratio of
+  // the image(1 over 1.5389558232931726)
+  image(img, 0, drawheight, sw, imgh);
 }
 
-console.log("hello");
+console.log("Making sketch.js changes");
+console.log("imgh = " + imgh);
+console.log("Image is being drawn within canvas starting at = " + drawheight);
+console.log("Image dimensions are " + img.height + img.width);
