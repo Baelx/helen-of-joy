20e83f60b2d91dc0861d40bef2d2c5ea957bfefe
Alex <awintschel@gmail.com>
2017-05-18 00:23:20 -0400

    x axis of text set

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-The image must be rendered at 1.538... times its width, which is set to the width of the window. Therefore, sh must equal 1.538... times sw.
+Hero image animation done with p5.js
 
-It's got to be half of the canvas height + the distance from the top of the image to the top of the canvas.
-
-canvas height = 800
-image height = 2460
-
-Halve them both and subtract the smaller from the larger
-
-400
-1230
-
-((sw * 1.538)/2) - sh/2
-
+Static site without any Wordpress integration
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -18,16 +18,18 @@ function setup() {
 
 function draw() {
   // Displays the image exactly as wide as the viewport and keeps the aspect ratio of
-  // the image(1 over 1.5389558232931726)
+  // the image(1 over 1.538)
   image(img, 0, -drawheight, sw, imgh);
+  // clickable text links below
   fill('white');
   textFont(moonflower, 50);
-  text(links[0], 410, 170, 70, 80);
-  text(links[1], 1125, 180, 70, 80);
-  text(links[2], 280, 640, 180, 180);
-  text(links[3], 685, 575, 70, 80);
-  text(links[4], 1065, 660, 70, 80);
+  text(links[0], sw / 3.902, imgh / 14.470, 70, 80); // 2460 / 170 = 14.470
+  text(links[1], sw / 1.422, imgh / 13.666, 70, 80);
+  text(links[2], sw / 5.714, imgh / 3.843, 180, 180);
+  text(links[3], sw / 2.335, imgh / 4.278, 70, 80);
+  text(links[4], sw / 1.502, imgh / 3.72727, 70, 80);
 }
 
 //debugging section
 console.log("Making sketch.js changes");
+console.log(imgh / 3.72727);
