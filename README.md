aa355a9737e3316e90441e17b1aaecff5c287932
Alex <awintschel@gmail.com>
2017-05-17 22:44:50 -0400

    Image is now vertically and horizontally centered within canvas at all resolutions

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# helen-of-joy
+The image must be rendered at 1.538... times its width, which is set to the width of the window. Therefore, sh must equal 1.538... times sw.
 
-For testing, spin up a local server with NodeJS, using the http-server command. Run "npm install -g http-server" on a machine to get started.
+It's got to be half of the canvas height + the distance from the top of the image to the top of the canvas.
 
-Image map done using p5.js
+canvas height = 800
+image height = 2460
+
+Halve them both and subtract the smaller from the larger
+
+400
+1230
+
+((sw * 1.538)/2) - sh/2
+
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,22 +1,33 @@
-var img;  // Declare variable 'img'.
+var img;
+var links = ["AUDIO","VIDEO","UPCOMING SHOWS","BLOG","CONTACT"];
 var sw = window.innerWidth;
 var sh = window.innerHeight;
-var imgh = sh * 1.5389558232931726;
-var drawheight = imgh * -0.25;
+var imgh = sw * 1.538;
+var drawheight = imgh/2 - sh/2;
+var moonflower;
+
+function preload() {
+  moonflower = loadFont('assets/fonts/moon-flower-bold.ttf');
+}
 
 function setup() {
   var cnv = createCanvas(sw, sh);
-  cnv.parent('hero'); 
+  cnv.parent('hero');
   img = loadImage("assets/img/helenofjoytall.jpg");
 }
 
 function draw() {
   // Displays the image exactly as wide as the viewport and keeps the aspect ratio of
   // the image(1 over 1.5389558232931726)
-  image(img, 0, drawheight, sw, imgh);
+  image(img, 0, -drawheight, sw, imgh);
+  fill('white');
+  textFont(moonflower, 50);
+  text(links[0], 410, 170, 70, 80);
+  text(links[1], 1125, 180, 70, 80);
+  text(links[2], 280, 640, 180, 180);
+  text(links[3], 685, 575, 70, 80);
+  text(links[4], 1065, 660, 70, 80);
 }
 
+//debugging section
 console.log("Making sketch.js changes");
-console.log("imgh = " + imgh);
-console.log("Image is being drawn within canvas starting at = " + drawheight);
-console.log("Image dimensions are " + img.height + img.width);
