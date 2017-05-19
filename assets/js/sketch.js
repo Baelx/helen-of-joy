9d6f7acd3cae1374b5ecc8ea8368cf2af89da9e7
Alex <awintschel@gmail.com>
2017-05-19 00:26:56 -0400

    further tweaking the image map

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,10 +1,20 @@
 var img;
-var links = ["AUDIO","VIDEO","UPCOMING SHOWS","BLOG","CONTACT"];
+var links = ["AUDIO","VIDEO","SHOWS","BLOG","CONTACT"];
 var sw = window.innerWidth;
 var sh = window.innerHeight;
-var imgh = sw * 1.538;
-var drawheight = imgh/2 - sh/2;
+var imgh = sw * 1.5389558232931726;
+var drawheight = -(imgh/2 - sh/2);
 var moonflower;
+
+var fontDraw = function() {
+if(sw >= 1200){
+return imgh/49;
+} else if (sw >= 700) {
+return imgh/35;
+} else {
+return imgh/25;
+  }
+}
 
 function preload() {
   moonflower = loadFont('assets/fonts/moon-flower-bold.ttf');
@@ -18,16 +28,17 @@ function setup() {
 
 function draw() {
   // Displays the image exactly as wide as the viewport and keeps the aspect ratio of
-  // the image(1 over 1.538)
-  image(img, 0, -drawheight, sw, imgh);
+  // the image(1 over 1.5389558232931726)
+  image(img, 0, drawheight, sw, imgh);
   // clickable text links below
   fill('white');
-  textFont(moonflower, 50);
-  text(links[0], sw / 3.902, imgh / 14.470, 70, 80); // 2460 / 170 = 14.470
-  text(links[1], sw / 1.422, imgh / 13.666, 70, 80);
-  text(links[2], sw / 5.714, imgh / 3.843, 180, 180);
-  text(links[3], sw / 2.335, imgh / 4.278, 70, 80);
-  text(links[4], sw / 1.502, imgh / 3.72727, 70, 80);
+  textFont(moonflower);
+  textSize(fontDraw());
+  text(links[0], sw / 3.9036, 170, 70, 80);
+  text(links[1], sw / 1.4234, 180, 70, 80);
+  text(links[2], sw / 5.5154, 640, 70, 80);
+  text(links[3], sw / 2.3369, 575, 70, 80);
+  text(links[4], sw / 1.5035, 660, 70, 80);
 }
 
 //debugging section
