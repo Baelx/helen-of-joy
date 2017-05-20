2cfae0cbdf97912901006bcea480a9e6f0ed1442
Alex <awintschel@gmail.com>
2017-05-20 00:56:20 -0400

    Added circle links

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,10 +1,10 @@
 var img;
-var links = ["AUDIO","VIDEO","SHOWS","BLOG","CONTACT"];
+var sections = ["AUDIO","VIDEO","SHOWS","BLOG","CONTACT"];
 var sw = window.innerWidth;
 var sh = window.innerHeight;
 var imgh = sw * 1.5389558232931726;
 var drawheight = -(imgh/2 - sh/2);
-var moonflower;
+var moonflower; //font
 
 var fontDraw = function() {
 if(sw >= 1200){
@@ -30,15 +30,51 @@ function draw() {
   // Displays the image exactly as wide as the viewport and keeps the aspect ratio of
   // the image(1 over 1.5389558232931726)
   image(img, 0, drawheight, sw, imgh);
-  // clickable text links below
+  //  text that will fade in
   fill('white');
   textFont(moonflower);
   textSize(fontDraw());
-  text(links[0], sw / 3.9036, 170, 70, 80);
-  text(links[1], sw / 1.4234, 180, 70, 80);
-  text(links[2], sw / 5.5154, 640, 70, 80);
-  text(links[3], sw / 2.3369, 575, 70, 80);
-  text(links[4], sw / 1.5035, 660, 70, 80);
+  text(sections[0], sw / 3.9036, 170, 70, 80);
+  text(sections[1], sw / 1.4234, 180, 70, 80);
+  text(sections[2], sw / 5.5154, 640, 70, 80);
+  text(sections[3], sw / 2.3369, 575, 70, 80);
+  text(sections[4], sw / 1.5035, 660, 70, 80);
+
+  // clickable circles over the stars
+  ellipse(450,155,27);  // AUDIO
+  ellipse(1157,166,27); // VIDEO
+  ellipse(338,626,27);  // SHOWS
+  ellipse(713,564,27);  // BLOG
+  ellipse(1115,647,27); // CONTACT
+
+  mouseHover();
+
+}
+
+function mouseHover() {
+  // Check if mouse is inside the circles
+  var withina = dist(mouseX, mouseY, 450, 155);
+  var withinv = dist(mouseX, mouseY, 1157, 166);
+  var withins = dist(mouseX, mouseY, 338, 626);
+  var withinb = dist(mouseX, mouseY, 713, 564);
+  var withinc = dist(mouseX, mouseY, 1115, 647);
+  if (withina < 13.5 || withinv < 13.5 || withins < 13.5 || withinb < 13.5 || withinc < 13.5) {
+    document.body.style.cursor = "pointer";
+  }
+}
+
+
+
+function mousePressed() {
+  // Check if mouse is inside the circles
+  var withina = dist(mouseX, mouseY, 450, 155);
+  var withinv = dist(mouseX, mouseY, 1157, 166);
+  var withins = dist(mouseX, mouseY, 338, 626);
+  var withinb = dist(mouseX, mouseY, 713, 564);
+  var withinc = dist(mouseX, mouseY, 1115, 647);
+  if (withina < 13.5 || withinv < 13.5 || withins < 13.5 || withinb < 13.5 || withinc < 13.5) {
+    console.log('clicked within a circle');
+  }
 }
 
 //debugging section
