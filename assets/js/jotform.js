f8a5c28208d10846d298ec5a49af0b6a3d1e0aed
Alex <awintschel@gmail.com>
2017-05-21 14:49:12 -0400

    Added contact page with jotform

---
+++ assets/js/jotform.js
@@ -0,0 +1,15 @@
+
+var ifr = document.getElementById("JotFormIFrame-71405193502246");
+if(window.location.href && window.location.href.indexOf("?") > -1) { var get = window.location.href.substr(window.location.href.indexOf("?") + 1);
+if(ifr && get.length > 0) { var src = ifr.src; src = src.indexOf("?") > -1 ? src + "&" + get : src + "?" + get; ifr.src = src; } }
+window.handleIFrameMessage = function(e) { var args = e.data.split(":"); if (args.length > 2)
+{ iframe = document.getElementById("JotFormIFrame-" + args[2]); } else { iframe = document.getElementById("JotFormIFrame"); }
+if (!iframe) return; switch (args[0]) { case "scrollIntoView": iframe.scrollIntoView(); break; case "setHeight": iframe.style.height = args[1] + "px";
+break; case "collapseErrorPage": if (iframe.clientHeight > window.innerHeight) { iframe.style.height = window.innerHeight + "px"; }
+break; case "reloadPage": window.location.reload(); break; } var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
+if(isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
+  var urls = {"docurl":encodeURIComponent(document.URL),"referrer":encodeURIComponent(document.referrer)};
+  iframe.contentWindow.postMessage(JSON.stringify({"type":"urls","value":urls}), "*"); } };
+  if (window.addEventListener) { window.addEventListener("message", handleIFrameMessage, false); }
+  else if (window.attachEvent) { window.attachEvent("onmessage", handleIFrameMessage); }
+  
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -5,6 +5,8 @@ var sh = window.innerHeight;
 var imgh = sw * 1.5389558232931726;
 var drawheight = -(imgh/2 - sh/2);
 var moonflower; //font
+var incircles = [];
+var circlew;
 
 var fontDraw = function() {
 if(sw >= 1200){
@@ -18,18 +20,25 @@ return imgh/25;
 
 function preload() {
   moonflower = loadFont('assets/fonts/moon-flower-bold.ttf');
+  img = loadImage("assets/img/helenofjoytall.jpg");
 }
 
 function setup() {
   var cnv = createCanvas(sw, sh);
   cnv.parent('hero');
-  img = loadImage("assets/img/helenofjoytall.jpg");
-}
 
-function draw() {
   // Displays the image exactly as wide as the viewport and keeps the aspect ratio of
   // the image(1 over 1.5389558232931726)
   image(img, 0, drawheight, sw, imgh);
+
+  circlew = 27;
+  // clickable circles over the stars
+  ellipse(450,155,circlew);  // AUDIO
+  ellipse(1157,166,circlew); // VIDEO
+  ellipse(338,626,circlew);  // SHOWS
+  ellipse(713,564,circlew);  // BLOG
+  ellipse(1115,647,circlew); // CONTACT
+
   //  text that will fade in
   fill('white');
   textFont(moonflower);
@@ -39,44 +48,57 @@ function draw() {
   text(sections[2], sw / 5.5154, 640, 70, 80);
   text(sections[3], sw / 2.3369, 575, 70, 80);
   text(sections[4], sw / 1.5035, 660, 70, 80);
+}
 
-  // clickable circles over the stars
-  ellipse(450,155,27);  // AUDIO
-  ellipse(1157,166,27); // VIDEO
-  ellipse(338,626,27);  // SHOWS
-  ellipse(713,564,27);  // BLOG
-  ellipse(1115,647,27); // CONTACT
-
-  mouseHover();
+function draw() {
 
 }
 
-function mouseHover() {
+function mouseMoved() {
   // Check if mouse is inside the circles
-  var withina = dist(mouseX, mouseY, 450, 155);
-  var withinv = dist(mouseX, mouseY, 1157, 166);
-  var withins = dist(mouseX, mouseY, 338, 626);
-  var withinb = dist(mouseX, mouseY, 713, 564);
-  var withinc = dist(mouseX, mouseY, 1115, 647);
-  if (withina < 13.5 || withinv < 13.5 || withins < 13.5 || withinb < 13.5 || withinc < 13.5) {
-    document.body.style.cursor = "pointer";
-  }
+  console.log("mouseMoved running");
+  // Check if mouse is inside the circles
+  incircles[0] = dist(mouseX, mouseY, 450, 155);
+  incircles[1] = dist(mouseX, mouseY, 1157, 166);
+  incircles[2] = dist(mouseX, mouseY, 338, 626);
+  incircles[3] = dist(mouseX, mouseY, 713, 564);
+  incircles[4] = dist(mouseX, mouseY, 1115, 647);
+
+
+
+    if (incircles < circlew/2)
+        {
+      document.body.style.cursor = "pointer";
+      } else {
+      document.body.style.cursor = "default";
+        }
+
+
+
+
+
+
+
+
 }
 
 
 
 function mousePressed() {
+  console.log("mousePressed running")
   // Check if mouse is inside the circles
-  var withina = dist(mouseX, mouseY, 450, 155);
-  var withinv = dist(mouseX, mouseY, 1157, 166);
-  var withins = dist(mouseX, mouseY, 338, 626);
-  var withinb = dist(mouseX, mouseY, 713, 564);
-  var withinc = dist(mouseX, mouseY, 1115, 647);
-  if (withina < 13.5 || withinv < 13.5 || withins < 13.5 || withinb < 13.5 || withinc < 13.5) {
-    console.log('clicked within a circle');
-  }
+  incircles[0] = dist(mouseX, mouseY, 450, 155);
+  incircles[1] = dist(mouseX, mouseY, 1157, 166);
+  incircles[2] = dist(mouseX, mouseY, 338, 626);
+  incircles[3] = dist(mouseX, mouseY, 713, 564);
+  incircles[4] = dist(mouseX, mouseY, 1115, 647);
+
+  incircles.forEach(function(x) {
+  if (x < circlew/2){
+    return incircles[x];
+    }
+  })
 }
 
 //debugging section
 console.log("Making sketch.js changes");
-console.log(imgh / 3.72727);
