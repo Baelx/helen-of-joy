4b6bf2ddcb0c8f92dd17c77763a04cc4c516a4ce
Alex <awintschel@gmail.com>
2017-05-22 17:17:00 -0400

    Circles and text fully responsive

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -6,7 +6,8 @@ var imgh = sw * 1.5389558232931726;
 var drawheight = -(imgh / 2 - sh / 2);
 var moonflower; //font
 var incircles = [];
-var circlew;
+var circlew = 27;
+var textOpacity = 0;
 
 var fontDraw = function() {
   if (sw >= 1200) {
@@ -18,12 +19,12 @@ var fontDraw = function() {
   }
 }
 var circleDraw = function() {
-if(sw >=1000){
-return circlew;
-} else if (sw < 1000) {
-  return circlew-7;
-} else if (sw < 800) {
-  return circlew-10;
+  if (sw >= 1000) {
+    return circlew;
+  } else if (sw < 1000) {
+    return circlew - 7;
+  } else {
+    return circlew - 15;
   }
 }
 
@@ -40,37 +41,39 @@ function setup() {
   image(img, 0, drawheight, sw, sw * 1.5389558232931726);
 
 
-  circlew = 27;
   // clickable circles over the stars
   noStroke();
-  ellipse(sw / 3.5555, imgh/2.4981410215926707 + drawheight, circleDraw()); // AUDIO
-  ellipse(sw / 1.38288, imgh/2.4705695099518676 + drawheight, circleDraw()); // VIDEO
-  ellipse(sw / 4.73372, imgh/1.6903885892153363 + drawheight, circleDraw()); // SHOWS
-  ellipse(sw / 2.24403, imgh/1.7655350352677948 + drawheight, circleDraw()); // BLOG
-  ellipse(sw / 1.434977, imgh/1.666365438789363 + drawheight, circleDraw()); // CONTACT
+  ellipse(sw / 3.5555, imgh / 2.4981410215926707 + drawheight, circleDraw()); // AUDIO
+  ellipse(sw / 1.38288, imgh / 2.4705695099518676 + drawheight, circleDraw()); // VIDEO
+  ellipse(sw / 4.73372, imgh / 1.6903885892153363 + drawheight, circleDraw()); // SHOWS
+  ellipse(sw / 2.24403, imgh / 1.7655350352677948 + drawheight, circleDraw()); // BLOG
+  ellipse(sw / 1.434977, imgh / 1.666365438789363 + drawheight, circleDraw()); // CONTACT
 
   //  text that will fade in
-  fill('white');
+
+  fill(255, 255, 255, textOpacity);
   textFont(moonflower);
+  tint(255, 126);
   textSize(fontDraw());
-  text(sections[0], sw / 3.9036, imgh/2.460693795890683 + drawheight, 70, 80);
-  text(sections[1], sw / 1.4234, imgh/2.4363465133883424 + drawheight, 70, 80);
-  text(sections[2], sw / 5.5154, imgh/1.6742969260954872 + drawheight, 70, 80);
-  text(sections[3], sw / 2.3369, imgh/1.7517188770051184 + drawheight, 70, 80);
-  text(sections[4], sw / 1.5035, imgh/1.6518331624797433 + drawheight, 70, 80);
+  text(sections[0], sw / 3.9036, imgh / 2.460693795890683 + drawheight, 70, 80);
+  text(sections[1], sw / 1.4234, imgh / 2.4363465133883424 + drawheight, 70, 80);
+  text(sections[2], sw / 5.5154, imgh / 1.6742969260954872 + drawheight, 70, 80);
+  text(sections[3], sw / 2.3369, imgh / 1.7517188770051184 + drawheight, 70, 80);
+  text(sections[4], sw / 1.5035, imgh / 1.6518331624797433 + drawheight, 70, 80);
+
 }
 
-function draw() {
+function Draw() {
 
 }
 
 function mouseMoved() {
   // Check if mouse is inside the circles
-  incircles[0] = dist(mouseX, mouseY, sw / 3.5555, imgh/2.4981410215926707 + drawheight);
-  incircles[1] = dist(mouseX, mouseY, sw / 1.38288, imgh/2.4705695099518676 + drawheight);
-  incircles[2] = dist(mouseX, mouseY, sw / 4.73372, imgh/1.6903885892153363 + drawheight);
-  incircles[3] = dist(mouseX, mouseY, sw / 2.24403, imgh/1.7655350352677948 + drawheight);
-  incircles[4] = dist(mouseX, mouseY, sw / 1.434977, imgh/1.666365438789363 + drawheight);
+  incircles[0] = dist(mouseX, mouseY, sw / 3.5555, imgh / 2.4981410215926707 + drawheight);
+  incircles[1] = dist(mouseX, mouseY, sw / 1.38288, imgh / 2.4705695099518676 + drawheight);
+  incircles[2] = dist(mouseX, mouseY, sw / 4.73372, imgh / 1.6903885892153363 + drawheight);
+  incircles[3] = dist(mouseX, mouseY, sw / 2.24403, imgh / 1.7655350352677948 + drawheight);
+  incircles[4] = dist(mouseX, mouseY, sw / 1.434977, imgh / 1.666365438789363 + drawheight);
 
   if (incircles[0] < circleDraw() / 2 || incircles[1] < circleDraw() / 2 || incircles[2] < circleDraw() / 2 || incircles[3] < circleDraw() / 2 || incircles[4] < circleDraw() / 2) {
     document.body.style.cursor = "pointer";
@@ -83,11 +86,11 @@ function mouseMoved() {
 
 function mousePressed() {
   // Check if mouse is inside the circles
-  incircles[0] = dist(mouseX, mouseY, sw / 3.5555, imgh/2.4981410215926707 + drawheight);
-  incircles[1] = dist(mouseX, mouseY, sw / 1.38288, imgh/2.4705695099518676 + drawheight);
-  incircles[2] = dist(mouseX, mouseY, sw / 4.73372, imgh/1.6903885892153363 + drawheight);
-  incircles[3] = dist(mouseX, mouseY, sw / 2.24403, imgh/1.7655350352677948 + drawheight);
-  incircles[4] = dist(mouseX, mouseY, sw / 1.434977, imgh/1.666365438789363 + drawheight);
+  incircles[0] = dist(mouseX, mouseY, sw / 3.5555, imgh / 2.4981410215926707 + drawheight);
+  incircles[1] = dist(mouseX, mouseY, sw / 1.38288, imgh / 2.4705695099518676 + drawheight);
+  incircles[2] = dist(mouseX, mouseY, sw / 4.73372, imgh / 1.6903885892153363 + drawheight);
+  incircles[3] = dist(mouseX, mouseY, sw / 2.24403, imgh / 1.7655350352677948 + drawheight);
+  incircles[4] = dist(mouseX, mouseY, sw / 1.434977, imgh / 1.666365438789363 + drawheight);
 
 
   if (incircles[0] < circleDraw() / 2) {
