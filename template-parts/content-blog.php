128e952f5d38747acb26955600422a037f31453a
Alex <awintschel@gmail.com>
2017-09-15 23:22:07 -0400

    recent blog posts loaded with get template part

---
+++ template-parts/content-blog.php
@@ -0,0 +1,65 @@
+<!--BLOG-->
+<section class="blog" id="blogsection">
+  <div class="container">
+    <div class="row section-header" id="primary">
+      <h2>RECENT BLOG ENTRIES</h2>
+      <main id="content" class="col-sm-6" role="main">
+        <article class="post">
+          <header>
+            <h3>
+              <a href="post.html">Blog Title Here
+              </a>
+            </h3>
+            <div class="post-details">
+              <i class="fa fa-user">Jess</i>
+              <i class="fa fa-clock-o">Jan 1, 2017</i>
+              <i class="fa fa-folder"></i>
+              <a href="">Tutorials</a>
+              <a href="">Coding</a>
+              <i class="fa fa-tags"></i>
+              <a href="">premium</a><a href="">tag   </a><a href="">another tag</a><a href="">sup</a>
+              <div class="post-comments-badge">
+                <a href=""><i class="fa fa-comments">168</i></a>
+              </div>
+            </div>
+          </header>
+          <div class="post-image">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/hero-bg.jpg" alt="Hero image">
+          </div>
+          <div class="post-excerpt">
+            <p>This is the sample blog excerpt for your viewing pleasure. You can read more if you just click on the damn blog you jerk...<a href="post.html">continue reading &raquo;</a></p>
+          </div>
+        </article>
+      </main>
+      <main id="content" class="col-sm-6" role="main">
+        <article class="post">
+          <header>
+            <h3>
+              <a href="post.html">Blog Title Here
+              </a>
+            </h3>
+            <div class="post-details">
+              <<i class="fa fa-user">Jess</i>
+              <i class="fa fa-clock-o">Jan 1, 2017</i>
+              <i class="fa fa-folder"></i>
+              <a href="">Tutorials</a>
+              <a href="">Coding</a>
+              <i class="fa fa-tags"></i>
+              <a href="">premium</a><a href="">tag   </a><a href="">another tag</a><a href="">sup</a>
+              <div class="post-comments-badge">
+                <a href=""><i class="fa fa-comments">168</i></a>
+              </div>
+            </div>
+          </header>
+          <div class="post-image">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/hero-bg.jpg" alt="Hero image">
+          </div>
+          <div class="post-excerpt">
+            <p>This is the sample blog excerpt for your viewing pleasure. You can read more if you just click on the damn blog you jerk...<a href="post.html">continue reading &raquo;</a></p>
+          </div>
+        </article>
+      </main>
+      <a href="blog.html" class="link-to-more">Read More...</a>
+    </div>
+  </div>
+</section>
