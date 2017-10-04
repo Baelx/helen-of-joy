c512191334db04da37daaf6a7c15b450678b8910
Alex <awintschel@gmail.com>
2017-10-03 21:57:00 -0400

    tying in blog.html code within content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -19,9 +19,17 @@
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php helen_of_joy_posted_on(); ?>
-		</div><!-- .entry-meta -->
+			<div class="post-details">
+				<i class="fa fa-user"></i> Brad Hussey
+				<i class="fa fa-clock-o"></i> <time>August 7, 2014</time>
+				<i class="fa fa-folder"></i> <a href="">Tutorials</a>, <a href="">Coding</a>
+				<i class="fa fa-tags"></i> Tagged <a href="">wordpress</a>, <a href="">premium</a>, <a href="">another tag</a>, <a href="">yadda yadaa</a>
+
+				<div class="post-comments-badge">
+					<a href=""><i class="fa fa-comments"></i> 168</a>
+				</div><!-- post-comments-badge -->
+			</div><!-- post-details -->
+
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
