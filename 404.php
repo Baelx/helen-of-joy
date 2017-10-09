ece6cf52bbf57a1995b7b1636b09dbfb96528883
Alex <awintschel@gmail.com>
2017-10-08 23:17:45 -0400

    Added basic 404 page

--- 404.php
+++ 404.php
@@ -9,56 +9,21 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section>
+	<h1>Bummer dude</h1>
+</section>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'helen-of-joy' ); ?></h1>
-				</header><!-- .page-header -->
-
+<div class="container">
+	<div class="row" id="primary">
+		<div id="content" class="col-sm-12">
+			<div class="error-404 not-found">
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'helen-of-joy' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( helen_of_joy_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'helen-of-joy' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'helen-of-joy' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					<h2>Don't worry - there's no place like <a href="<?php echo esc_url(home_url('/'));?>">home</a>.</h2>
+				</div>
+			</div>
+		</div><!-- content -->
+	</div><!-- row -->
+</div><!-- container -->
 
 <?php
 get_footer();
