a54de033a7d04b87c252f93380bcf91354508659
Alex <awintschel@gmail.com>
2017-10-17 01:10:45 -0400

    further tweaks on 404 navbar

--- 404.php
+++ 404.php
@@ -17,5 +17,30 @@ get_header(); ?>
     1px 1px 0 #fff;">Page not found</h1>
 </section>
 
+<style>
+a, li.current_page_parent a, h2 {
+	color:black !important;
+}
+ul {
+	float:right;
+}
+.navbar-nav > li > a {
+	background-color:white;
+}
+@media screen and (max-width:430px) {
+ul {
+	border: 1px solid black;
+        border-top-width: 1px;
+        border-top-style: solid;
+        border-top-color: black;
+    margin-top: 0;
+    border-top: none;
+
+}
+}
+}
+
+</style>
+
 <?php
 get_footer();
