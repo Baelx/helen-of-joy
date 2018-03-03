eed5763b6df5783a5e97c9c49e48ae0b4a290471
Alex <awintschel@gmail.com>
2018-03-03 17:18:27 -0500

    added mobile background img field for homepage

--- template-parts/content-bio.php
+++ template-parts/content-bio.php
@@ -4,12 +4,22 @@
 $bio_title = get_field('bio_title');
 $bio_content = get_field('bio_content');
 $bio_background_image = get_field('bio_background_image');
-
+$bio_mobile_background = get_field("bio_mobile_background");
 ?>
 
 <!--BIO SECTION-->
-<section id="bio-section" class="section-block" style="background-image: url(<?php echo $bio_background_image['url'] ?>);"
-  title="<?php echo $bio_background_image['alt'] ?>">
+<style>
+#bio-section {
+background-image: url(<?php echo $bio_mobile_background['url'] ?>);
+}
+
+@media screen and (min-width:772px){
+  #bio-section {
+    background-image: url(<?php echo $bio_background_image['url'] ?>);
+  }
+}
+</style>
+<section id="bio-section" class="section-block" title="<?php echo $bio_background_image['alt'] ?>">
   <div class="container" id="bio-container">
     <div class="section-header">
       <h2 id="bio-header"><?php echo $bio_title; ?></h2>
--- template-parts/content-video.php
+++ template-parts/content-video.php
@@ -2,14 +2,25 @@
 
 $featured_video_section_title = get_field('featured_video_section_title');
 $featured_image = get_field('featured_video_background_image');
+$video_mobile_background = get_field('video_mobile_background');
 $video_html_embed_code = get_field('video_html_embed_code');
 $link_to_video_page_text = get_field('link_to_video_page_text');
 
  ?>
 
+ <style>
+ #featurette {
+ background-image:url(<?php echo $video_mobile_background['url'] ?>);
+ }
 
+ @media screen and (min-width:772px){
+   #featurette {
+    background-image:url(<?php echo $featured_image['url'] ?>);
+   }
+ }
+ </style>
 <!--VIDEO FEATURETTE-->
-<section id="featurette" class="section-block" style="background-image:url(<?php echo $featured_image['url'] ?>);">
+<section id="featurette" class="section-block">
   <div class="container">
     <div class="row section-header">
       <div class="col-sm-8 col-sm-offset-2">
