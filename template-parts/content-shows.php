5ab56684fe3374d8c7328c37d8e95a98a6d4fb11
Alex <awintschel@gmail.com>
2018-03-06 20:21:01 -0500

    shows should now appear in order of show date

--- template-parts/content-shows.php
+++ template-parts/content-shows.php
@@ -1,5 +1,6 @@
 <?php
 // Show Variables
+$show = "example";
 
 ?>
 
@@ -10,33 +11,58 @@
       <div class="col-sm-12 section-header">
         <h2>Shows</h2>
 
-        <?php $loop = new WP_Query(array('post_type' => 'show',
-          'orderby' => 'post_date' , 'order' => 'DESC')); ?>
 
+        <?php
 
-        <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+        // get posts
+        $posts = get_posts(array(
+          'post_type'			=> 'show',
+          'posts_per_page'	=> 4,
+          'meta_key'			=> 'show_date',
+          'order'				=> 'DESC'
+        ));
+
+        if( $posts ): ?>
+
+        <?php foreach( $posts as $post ):
+
+          setup_postdata( $post )
+
+          ?>
 
           <div class="row show">
             <div class="col-sm-6">
               <?php  if (  has_post_thumbnail() ) {//check for post image
-                     the_post_thumbnail( array(200,200) );
-                  }
+                the_post_thumbnail( array(200,200) );
+              }
               ?>
             </div>
             <div class="col-sm-4">
               <h3><?php the_title(); ?></h3>
               <p><?php the_content(); ?></p>
               <span id="show-date-time"><?php  $date = DateTime::createFromFormat('dmY', get_field('show_date')); echo $date->format('D jS F Y'); ?>, <?php the_field('show_time'); ?></br></span>
-              <span>
-              <a href="<?php esc_url(the_field('get_directions')); ?>">Get Directions</a>
-              -
-              <a href="<?php esc_url(the_field('event_page')); ?>">Event Page</a>
+              <span class="show-links">
+
+                
+                	<a href="<?php the_field('get_directions'); ?>">Get Directions</a>
+
+
+
+                	<a href="<?php the_field('event_page'); ?>">Event Page</a>
+
+
               </span>
             </div>
           </div>
-        <?php endwhile; ?>
 
-      </div>
+        <?php endforeach; ?>
+
+        <?php wp_reset_postdata(); ?>
+
+      <?php endif; ?>
+
+
     </div>
   </div>
+</div>
 </section>
